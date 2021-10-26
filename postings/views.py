fd3560ad5a7ea152d3b70fe8564cf61b15551e4b
kjhabc2002 <juhju2022@gmail.com>
2021-10-27 05:20:15 +0900

    Add : postings앱 게시판 등록,조회 기능구현

--- postings/views.py
+++ postings/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import json
+from json.decoder import JSONDecodeError
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.db    import transaction
+
+from postings.models import Posting
+from users.decorators  import login_decorator
+
+
+class PostingView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data    = json.loads(request.body)
+            user    = request.user
+            title   = data['title']
+            content = data['content']
+
+            Posting.objects.create(
+                user_id = user.id,
+                title   = title,
+                content = content
+            )
+            return JsonResponse({'data': 'SUCCESS'}, status=201)
+
+        except JSONDecodeError:
+            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        
+    def get(self, request):
+        try:
+            limit  = int(request.GET.get('limit', 10))
+            offset = int(request.GET.get('offset', 0))
+
+            if limit > 10:
+                return JsonResponse({'message':'TOO_MUCH_LIMIT'}, status=400)
+
+            postings  = Posting.objects.all()[offset:offset+limit]
+
+            result = [{
+                'post_id'    : posting.id,
+                'user'       : posting.user.name,
+                'title'      : posting.title,
+                "created_at" : posting.created_at,
+                "updated_at" : posting.updated_at
+                } for posting in postings]
+            
+            return JsonResponse({'count': len(postings), 'data':result}, status=200)
+
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
