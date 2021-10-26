65694b11c1cacf458b1978eba7de5666f9ead8c0
kjhabc2002 <juhju2022@gmail.com>
2021-10-27 04:39:14 +0900

    Add : migrate complete

--- prejuhyun/settings.py
+++ prejuhyun/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'core',
+    'users',
+    'postings'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +55,7 @@ MIDDLEWARE = [
    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'prejuhyun.urls'
